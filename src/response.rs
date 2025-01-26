c533869dcf826f3b74323779bad3bbff56b38cbe
Youlixx <combey.theo@hotmail.com>
2025-01-26 16:42:48 +0000

    Added a generic response schema

---
+++ src/response.rs
@@ -0,0 +1,73 @@
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct ApiError {
+    api_error_code: u8,
+    error_message: String,
+}
+
+#[derive(Serialize)]
+#[serde(tag = "status")]
+enum ApiResponseData<T: Serialize> {
+    Success(T),
+    Failure(ApiError),
+}
+
+pub struct ApiResponse<T: Serialize> {
+    status_code: StatusCode,
+    data: ApiResponseData<T>,
+}
+
+impl<T> IntoResponse for ApiResponse<T>
+where
+    T: Serialize,
+{
+    fn into_response(self) -> axum::response::Response {
+        (self.status_code, Json(self.data)).into_response()
+    }
+}
+
+impl<T> From<Result<(StatusCode, T), (StatusCode, ApiError)>> for ApiResponse<T>
+where
+    T: Serialize,
+{
+    fn from(value: Result<(StatusCode, T), (StatusCode, ApiError)>) -> Self {
+        match value {
+            Ok((status_code, value)) => Self {
+                status_code,
+                data: ApiResponseData::Success(value),
+            },
+            Err((status_code, error)) => Self {
+                status_code,
+                data: ApiResponseData::Failure(error),
+            },
+        }
+    }
+}
+
+impl<T> ApiResponse<T>
+where
+    T: Serialize,
+{
+    pub fn success(data: T) -> Self {
+        ApiResponse::success_with_status_code(StatusCode::OK, data)
+    }
+
+    pub fn success_with_status_code(status_code: StatusCode, data: T) -> Self {
+        Self {
+            status_code,
+            data: ApiResponseData::Success(data),
+        }
+    }
+
+    pub fn failure(status_code: StatusCode, api_error_code: u8, error_message: &str) -> Self {
+        Self {
+            status_code,
+            data: ApiResponseData::Failure(ApiError {
+                api_error_code,
+                error_message: error_message.to_string(),
+            }),
+        }
+    }
+}
