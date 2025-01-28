55399e9283f38eb50b22f85807f587340a9abcce
Youlixx <combey.theo@hotmail.com>
2025-01-28 22:55:40 +0000

    Clean up temp files

--- src/routes/files.rs
+++ src/routes/files.rs
@@ -1,12 +1,6 @@
-use std::{
-    io::{self, IoSlice},
-    mem::ManuallyDrop,
-    ops::{Deref, DerefMut},
-    path::{Path, PathBuf},
-    pin::Pin,
-    task::{Context, Poll},
-};
+use std::path::PathBuf;
 
+use async_tempfile::TempFile;
 use axum::{
     body::Body,
     extract::{Multipart, Path as ExtractPath, Query, State},
@@ -17,10 +11,10 @@ use md5::Md5;
 use serde::Deserialize;
 use sha2::{Digest, Sha256};
 use tokio::{
-    fs::{remove_file, File},
-    io::{AsyncRead, AsyncWrite, AsyncWriteExt, ReadBuf},
+    fs::File,
+    io::AsyncWriteExt,
 };
-use tokio_tar::{ArchiveBuilder, Builder};
+use tokio_tar::Builder;
 use tokio_util::io::ReaderStream;
 use utoipa::ToSchema;
 use utoipa_axum::{router::OpenApiRouter, routes};
@@ -76,8 +70,7 @@ async fn get_filesystem_at(
         FileList::None => return Err(Error::FileNotFound(PathBuf::from(path))),
         FileList::SingleFile(path) =>  Body::from_stream(ReaderStream::new(File::open(path).await?)),
         FileList::MultipleFile(files) => {
-            // TODO use an actual temp file, and uuid generated file name.
-            let buffer = File::create("/tmp/temp_archive.tar").await?;
+            let buffer = TempFile::new().await?;
             let mut builder = Builder::new(buffer);
 
             for (absolute_local_path, archive_path) in files {
@@ -85,19 +78,16 @@ async fn get_filesystem_at(
                 let mut file = File::open(absolute_local_path).await?;
                 builder.append_file(archive_path, &mut file).await?;
             }
-            builder.into_inner().await?;
 
-            let file = File::open("/tmp/temp_archive.tar").await?;
+            let file = builder.into_inner().await?.open_ro().await?;
             Body::from_stream(ReaderStream::new(file))
         }
     };
 
-    // TODO remove the except
     let response = Response::builder()
         .status(StatusCode::OK)
         .header(header::CONTENT_TYPE, "application/octet-stream")
-        .body(body)
-        .expect("Failed to build response");
+        .body(body)?;
 
     Ok(response)
 }
@@ -107,83 +97,6 @@ async fn get_filesystem_at(
 struct UploadFileRequest {
     #[schema(format = Binary, content_media_type = "application/octet-stream")]
     file: String,
-}
-
-struct TempFile {
-    path: PathBuf,
-    file: ManuallyDrop<File>,
-}
-
-impl TempFile {
-    async fn new(path: impl AsRef<Path>) -> Result<Self> {
-        let path = path.as_ref().to_path_buf();
-        Ok(Self {
-            file: ManuallyDrop::new(File::create(&path).await?),
-            path,
-        })
-    }
-
-    async fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
-        self.file.write_all(buf).await
-    }
-}
-
-impl Drop for TempFile {
-    fn drop(&mut self) {
-        unsafe {
-            ManuallyDrop::drop(&mut self.file);
-        }
-
-        let path = self.path.clone();
-        tokio::spawn(async move {
-            // TODO we could print out a warning if this fails.
-            remove_file(path)
-                .await
-                .expect("Failed to delete the temp file.");
-        });
-    }
-}
-
-impl AsyncRead for TempFile {
-    fn poll_read(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &mut ReadBuf<'_>,
-    ) -> Poll<io::Result<()>> {
-        Pin::new(self.file.deref_mut()).poll_read(cx, buf)
-    }
-}
-
-impl AsyncWrite for TempFile {
-    fn poll_write(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &[u8],
-    ) -> Poll<io::Result<usize>> {
-        Pin::new(self.file.deref_mut()).poll_write(cx, buf)
-    }
-
-    fn poll_flush(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<io::Result<()>> {
-        Pin::new(self.file.deref_mut()).poll_flush(cx)
-    }
-
-    fn poll_shutdown(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<io::Result<()>> {
-        Pin::new(self.file.deref_mut()).poll_shutdown(cx)
-    }
-
-    fn poll_write_vectored(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        bufs: &[IoSlice<'_>],
-    ) -> Poll<io::Result<usize>> {
-        Pin::new(self.file.deref_mut()).poll_write_vectored(cx, bufs)
-    }
 }
 
 #[derive(Default)]
@@ -242,34 +155,28 @@ async fn upload_file(
     ExtractPath(path): ExtractPath<String>,
     mut multipart: Multipart,
 ) -> ApiResult<()> {
-    // TODO generate unique path, maybe use uuid to ensure uniqueness.
-    let path_temp_file = "/tmp/test";
-    let mut temp_file = TempFile::new(path_temp_file).await?;
-
+    let mut temp_file = TempFile::new().await?;
     let mut file_info_builder = FileInfoBuilder::new();
     let mut hasher_md5 = Md5::new();
     let mut hasher_sha256 = Sha256::new();
 
     while let Some(mut field) = multipart.next_field().await? {
-        match field.name() {
-            Some("file") => {
-                let mut size_in_bytes = 0;
+        if let Some("file") = field.name() {
+            let mut size_in_bytes = 0;
 
-                while let Some(chunk) = field.chunk().await? {
-                    temp_file.write_all(&chunk).await?;
+            while let Some(chunk) = field.chunk().await? {
+                temp_file.write_all(&chunk).await?;
 
-                    size_in_bytes += chunk.len();
-                    hasher_sha256.update(&chunk);
-                    hasher_md5.update(chunk);
-                }
-
-                file_info_builder.size_in_bytes(size_in_bytes as i64);
-
-                if let Some(original_name) = field.file_name() {
-                    file_info_builder.original_name(original_name);
-                }
+                size_in_bytes += chunk.len();
+                hasher_sha256.update(&chunk);
+                hasher_md5.update(chunk);
             }
-            _ => (),
+
+            file_info_builder.size_in_bytes(size_in_bytes as i64);
+
+            if let Some(original_name) = field.file_name() {
+                file_info_builder.original_name(original_name);
+            }
         }
     }
 
@@ -279,7 +186,7 @@ async fn upload_file(
 
     state
         .add_new_file_to_storage(
-            path_temp_file,
+            temp_file.file_path(),
             &path,
             file_info_builder
                 .build()
