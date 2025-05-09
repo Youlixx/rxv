d955e254286587106e8079fc2bc88a4e2c983bdc
Youlixx <combey.theo@hotmail.com>
2025-05-09 23:32:20 +0000

    [refactor] Refactored the API endpoints with the database

---
+++ src/api/mod.rs
@@ -0,0 +1,29 @@
+use axum::Router;
+use utoipa::OpenApi;
+use utoipa_axum::router::OpenApiRouter;
+use utoipa_swagger_ui::SwaggerUi;
+
+use crate::database::FileDatabase;
+
+pub mod files;
+pub mod response;
+
+#[derive(OpenApi)]
+#[openapi(
+    tags(
+        (name = "files", description = "Todo items management API")
+    )
+)]
+struct ApiDoc;
+
+pub fn get_router(database: FileDatabase) -> Router {
+    // TODO: maybe use a Arc here? check if it is ok to clone database.database?
+    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
+        .merge(files::router(database.clone()))
+        .split_for_parts();
+
+    let router =
+        router.merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api.clone()));
+
+    router
+}
