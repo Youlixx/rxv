773d35dbc232dd1d1a1d6919a4c21e331708859d
Youlixx <combey.theo@hotmail.com>
2025-05-10 12:03:16 +0000

    [test] Added metadata getter unit tests

--- src/database/get_metadata.rs
+++ src/database/get_metadata.rs
@@ -7,6 +7,7 @@ use super::{
     virtual_path::VirtualPath,
 };
 
+#[derive(Debug, Eq, PartialEq)]
 pub struct PathMetadataPair {
     pub virtual_path: VirtualPath,
     pub metadata: FileMetadata,
@@ -74,7 +75,7 @@ impl FileDatabase {
         let timestamp_str = timestamp.to_rfc3339();
         let path_wildcard = virtual_path.match_pattern();
 
-        let query = sqlx::query!(
+        let files = sqlx::query!(
             r#"
             SELECT
                 files.original_file_name,
@@ -91,11 +92,15 @@ impl FileDatabase {
             timestamp_str,
             timestamp_str,
             path_wildcard
-        );
+        )
+        .fetch_all(&self.database)
+        .await?;
 
-        Ok(query
-            .fetch_all(&self.database)
-            .await?
+        if files.is_empty() {
+            return Err(Error::VirtualFileNotFound(virtual_path));
+        }
+
+        Ok(files
             .into_iter()
             .map(|file| -> Result<PathMetadataPair> {
                 Ok(PathMetadataPair {
@@ -109,5 +114,214 @@ impl FileDatabase {
                 })
             })
             .collect::<Result<Vec<_>>>()?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::database::{
+        error::{Error, Result},
+        get_metadata::PathMetadataPair,
+        save_file::FileMetadata,
+        tests::{FileOperation, get_hash, get_timestamp, setup_test_database},
+        virtual_path::VirtualPath,
+    };
+
+    /// Test to verify that getting a single file metadata returns the expected result.
+    #[tokio::test]
+    async fn test_get_file_metadata() -> Result<()> {
+        let path = VirtualPath::from("/my_files/helloworld.txt");
+        let original_file_name = "some_file.txt";
+        let content = b"hello world!".to_vec().into_boxed_slice();
+
+        let (_test_dir, database) = setup_test_database(vec![FileOperation::Save {
+            original_file_name: original_file_name.to_owned(),
+            virtual_path: path.clone(),
+            content: content.clone(),
+            timestamp: get_timestamp(0),
+        }])
+        .await?;
+
+        assert_eq!(
+            database
+                .get_file_metadata(path.clone(), get_timestamp(1))
+                .await?,
+            PathMetadataPair {
+                virtual_path: path,
+                metadata: FileMetadata {
+                    original_file_name: original_file_name.to_owned(),
+                    size_in_bytes: content.len(),
+                    hash: get_hash(&content)
+                },
+                upload_timestamp: get_timestamp(0)
+            }
+        );
+
+        Ok(())
+    }
+
+    /// Test to verify that trying to get the metadata of a non-existing file fails.
+    #[tokio::test]
+    async fn test_get_missing_file_metadata() -> Result<()> {
+        let path = VirtualPath::from("/my_files/helloworld.txt");
+        let (_test_dir, database) = setup_test_database(vec![]).await?;
+
+        let get_metadata_result = database
+            .get_file_metadata(path.clone(), get_timestamp(1))
+            .await;
+
+        assert!(get_metadata_result.is_err());
+
+        match get_metadata_result.err().unwrap() {
+            Error::VirtualFileNotFound(error_path) => assert_eq!(path, error_path),
+            _ => assert!(false),
+        }
+
+        Ok(())
+    }
+
+    /// Test to verify that trying to get the metadata of a directory fails.
+    #[tokio::test]
+    async fn test_get_file_metadata_directory() -> Result<()> {
+        let path = VirtualPath::from("/some/directory/");
+        let (_test_dir, database) = setup_test_database(vec![]).await?;
+
+        let get_metadata_result = database
+            .get_file_metadata(path.clone(), get_timestamp(1))
+            .await;
+
+        assert!(get_metadata_result.is_err());
+
+        match get_metadata_result.err().unwrap() {
+            Error::NotAVirtualFile(error_path) => assert_eq!(path, error_path),
+            _ => assert!(false),
+        }
+
+        Ok(())
+    }
+
+    /// Test to verify that trying to get the metadata of a non-live file fails.
+    #[tokio::test]
+    async fn test_get_missing_file_metadata_timestamp() -> Result<()> {
+        let path = VirtualPath::from("/my_files/helloworld.txt");
+        let original_file_name = "some_file.txt";
+        let content = b"hello world!".to_vec().into_boxed_slice();
+
+        let (_test_dir, database) = setup_test_database(vec![FileOperation::Save {
+            original_file_name: original_file_name.to_owned(),
+            virtual_path: path.clone(),
+            content: content.clone(),
+            timestamp: get_timestamp(1),
+        }])
+        .await?;
+
+        let get_metadata_result = database
+            .get_file_metadata(path.clone(), get_timestamp(0))
+            .await;
+
+        assert!(get_metadata_result.is_err());
+
+        match get_metadata_result.err().unwrap() {
+            Error::VirtualFileNotFound(error_path) => assert_eq!(path, error_path),
+            _ => assert!(false),
+        }
+
+        Ok(())
+    }
+
+    /// Test to verify that getting a directory tree returns the expected result.
+    #[tokio::test]
+    async fn test_get_tree_metadata() -> Result<()> {
+        let files: [(_, _, &[u8]); 3] = [
+            ("file1.txt", "my_files/helloworld.txt", b"hello world!"),
+            ("file2.txt", "my_files/some_file.txt", b"I'm a sample file!"),
+            (
+                "file3.txt",
+                "definitely_not_a_file.txt",
+                b"I'm not a file :)",
+            ),
+        ];
+
+        let (_test_dir, database) = setup_test_database(
+            files
+                .iter()
+                .map(|(filename, virtual_path, content)| FileOperation::Save {
+                    original_file_name: filename.to_string(),
+                    virtual_path: VirtualPath::from(virtual_path),
+                    content: content.to_vec().into_boxed_slice(),
+                    timestamp: get_timestamp(0),
+                })
+                .collect(),
+        )
+        .await?;
+
+        let expected_file_metadata = files
+            .into_iter()
+            .map(|(filename, virtual_path, content)| PathMetadataPair {
+                virtual_path: VirtualPath::from(virtual_path),
+                metadata: FileMetadata {
+                    original_file_name: filename.to_string(),
+                    size_in_bytes: content.len(),
+                    hash: get_hash(content),
+                },
+                upload_timestamp: get_timestamp(0),
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(
+            database
+                .get_tree_metadata(VirtualPath::from("/my_files/"), get_timestamp(1))
+                .await?,
+            expected_file_metadata[..2]
+        );
+
+        assert_eq!(
+            database
+                .get_tree_metadata(VirtualPath::default(), get_timestamp(1))
+                .await?,
+            expected_file_metadata
+        );
+
+        Ok(())
+    }
+
+    /// Test to verify that trying to get the tree of a non-existing directory fails.
+    #[tokio::test]
+    async fn test_get_missing_directory_metadata() -> Result<()> {
+        let path = VirtualPath::from("/my_files/");
+        let (_test_dir, database) = setup_test_database(vec![]).await?;
+
+        let get_metadata_result = database
+            .get_tree_metadata(path.clone(), get_timestamp(1))
+            .await;
+
+        assert!(get_metadata_result.is_err());
+
+        match get_metadata_result.err().unwrap() {
+            Error::VirtualFileNotFound(error_path) => assert_eq!(path, error_path),
+            _ => assert!(false),
+        }
+
+        Ok(())
+    }
+
+    /// Test to verify that trying to get the tree of a file fails.
+    #[tokio::test]
+    async fn test_get_tree_metadata_file() -> Result<()> {
+        let path = VirtualPath::from("/some/file");
+        let (_test_dir, database) = setup_test_database(vec![]).await?;
+
+        let get_metadata_result = database
+            .get_tree_metadata(path.clone(), get_timestamp(1))
+            .await;
+
+        assert!(get_metadata_result.is_err());
+
+        match get_metadata_result.err().unwrap() {
+            Error::NotAVirtualDirectory(error_path) => assert_eq!(path, error_path),
+            _ => assert!(false),
+        }
+
+        Ok(())
     }
 }
